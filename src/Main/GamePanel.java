61992bfa33ec8b12b3992e7a724997df88eebaac
RUSHILPATEL33 <rushilp274@gmail.com>
2025-09-20 18:51:49 +0530

    added tiles and map

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,6 +1,7 @@
 package Main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,20 +13,19 @@ public class GamePanel extends JPanel implements  Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTilesSize * scale; //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; //768pixels
-    final int screenHeight = tileSize * maxScreenRow;//576 pixels
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize * maxScreenRow;//576 pixels
 
     int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
 
-    //set players default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
@@ -68,6 +68,7 @@ public class GamePanel extends JPanel implements  Runnable {
     public void paintComponent(Graphics g){
           super.paintComponent(g);
           Graphics2D g2 = (Graphics2D)g;
+          tileM.draw(g2);
 
           player.draw(g2);
 
